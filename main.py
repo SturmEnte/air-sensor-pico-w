c8b2dbe4a0683bb91eecbe35f054cc1a2be7a21c
Jonas <sturmente@proton.me>
2023-04-28 17:54:52 +0200

    Added a display to display the temp and humidity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from machine import Pin, I2C
 import utime as time
 from HDC1080 import HDC1080
+import sh1106
 import network
 import secrets
 import time
@@ -17,8 +18,20 @@ except:
 
 led = machine.Pin('LED', machine.Pin.OUT)
 led.on()
-    
-sensor = HDC1080(I2C(1, sda=Pin(26), scl=Pin(27)))
+
+i2c = I2C(1, sda=Pin(26), scl=Pin(27))
+
+sensor = HDC1080(i2c)
+display = sh1106.SH1106_I2C(128, 64, i2c, machine.Pin(2), 0x3c)
+
+def readSensor():
+    data = sensor.readSensor()
+    raw_temp = data[0]
+    raw_humid = data[1]
+    absolute_humid = absolute_from_relative_humidity(raw_temp, raw_humid)
+    corrected_temp = data[0] + TEMP_CORRECTION
+    corrected_humid = relative_from_absolute_humidity(corrected_temp, absolute_humid)
+    return (corrected_temp, corrected_humid, absolute_humid)
 
 # This formula is not 100% correct because it is not using the air pressure
 # https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
@@ -73,21 +86,34 @@ s.listen(1)
 while True:
     check_wifi()
     
-    s.settimeout(60)
+    try:
+        data = readSensor()
+    
+        display.sleep(False)
+        display.fill(0)
+
+        display.text(str(round(data[0], 2)), 0, 0, 1)
+        display.text('C', 50, 0, 1)
+        display.text(str(round(data[1], 2)), 0, 10, 1)
+        display.text('%', 50, 10, 1)
+        display.text(str(round(data[2], 2)), 0, 20, 1)
+        display.text('g/m3', 50, 20, 1)
+        display.show()
+    
+        display.show()
+    except:
+        print("Failed to read data or to display it on the screen")
+    
+    s.settimeout(10)
     try:
         cl, addr = s.accept()
         print("Request from: ", addr)
         request = cl.recv(1024)
         
         try:
-            data = sensor.readSensor()
-            raw_temp = data[0]
-            raw_humid = data[1]
-            absolute_humid = absolute_from_relative_humidity(raw_temp, raw_humid)
-            corrected_temp = data[0] + TEMP_CORRECTION
-            corrected_humid = relative_from_absolute_humidity(corrected_temp, absolute_humid)
+            data = readSensor()
             cl.send("HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n")
-            cl.send("{\"raw_temperature\":%s, \"raw_relative_humidity\":%s, \"temperature\":%s, \"relative_humidity\":%s, \"absolute_humidity\":%s,}" % (raw_temp, raw_humid, corrected_temp, corrected_humid, absolute_humid))
+            cl.send("{\"temperature\":%s, \"relative_humidity\":%s, \"absolute_humidity\":%s,}" % (data[0], data[1], data[2]))
     
         except:
             cl.send("HTTP/1.0 500 Internal Server Error\r\nContent-type: application/json\r\n\r\n")
